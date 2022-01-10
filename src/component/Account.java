32dabfd51e78021045ba614a3599e1b3aff98f2e
ArnaudSushi <arnaud.vadez@gmail.com>
2022-01-10 09:57:39 +0100

    Savings Account & Current Account created

---
+++ src/component/Account.java
@@ -0,0 +1,50 @@
+package component;
+
+//1.2.1 Creation of the account class
+public abstract class Account {
+	
+	protected String label;
+	protected int balance;
+	protected int accountId;
+	protected static int accountNumber;
+	protected Client client;
+	
+	public Account(String label, Client client) {
+		accountNumber++;
+		this.label = label;
+		this.client = client;
+		this.accountId = accountNumber;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int amount) {
+		this.balance = amount;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+}
---
+++ src/component/SavingsAccount.java
@@ -0,0 +1,9 @@
+package component;
+
+//1.2.2 Creation of the CurrentAccount and SavingsAccount
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+	}
+}
